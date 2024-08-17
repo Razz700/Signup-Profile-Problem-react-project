877ac6e3288c561e287bdf88e06227e2927ca3a5
Razzakh <mrazzakh123@gmail.com>
2024-08-17 17:03:39 +0530

    deploy message

--- src/App.js
+++ src/App.js
@@ -4,15 +4,13 @@ import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
 import Signup from './Pages/SignupPage'
 import ProfilePage from './Pages/ProfilePage'
 import LoginPage from './Pages/LoginPage'
-import { useDispatch, useSelector } from 'react-redux'
+import { useDispatch } from 'react-redux'
 import { isLoggedInCase } from './state/userAuthSlice'
 
 
 function App() {
-  const isLoggedIn=useSelector((state)=>state.userAuth.isLoggedIn);
   const dispatch=useDispatch();
-  const user=useSelector((state)=>state.userAuth.user);
-  
+
   /////////////////////////////////////
   useEffect(()=>{
     const checkValue=JSON.parse(localStorage.getItem('activeUser')??null);
